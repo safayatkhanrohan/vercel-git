b6c68f17bfa64a8f04d60a0bd878cd765702eb74
Safayat Khan Rohan <safayatkhanrohan@gmail.com>
2024-10-17 00:50:49 +0600

    iniital commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Hello World!'
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
